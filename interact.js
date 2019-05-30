3cfa039bf4758b7d61348b6d54ff90f33411f73f
Mehmet Ali ERGÜL <mehmetergul@std.iyte.edu.tr>
2019-05-30 17:53:02 +0300

    interacta bakilicak

--- interact.js
+++ interact.js
@@ -88,3 +88,40 @@ function addcontentmanager() {
 	    return obj;
 	}, {});
 }
+
+
+function selectCourseClicked() {
+	var data = $('form').serializeArray().reduce(function(obj, item) {
+	    obj[item.name] = item.value;
+	    return obj;
+	}, {});
+	
+	//Make checkboxes 1 or 0
+	if(data.active=="on") data.active=1;
+	else data.active=0;
+	
+	if(data.mandatory=="on") data.mandatory=1;
+	else data.mandatory=0;
+	
+	//Get all instructors from inputs
+	instructorArray=[];
+	xi=1;
+	while(data["instructors"+xi] != undefined){
+		instructorArray.push(data["instructors"+xi]);
+		xi++;
+	}
+
+	//Get all instructors from inputs
+	assistantArray=[];
+	xi=1;
+	while(data["assistants"+xi] != undefined){
+		assistantArray.push(data["assistants"+xi]);
+		xi++;
+	}
+	var instructorsObject = new InstructorClass(instructorArray);
+	var assistantsObject = new AssistantClass(assistantArray);
+	var courseObject = new CourseClass(data.courseName,data.courseCode,data.courseCredit,data.courseEcts,data.coursePrequirities,data.mandatory,data.active,data.semester,instructorsObject,assistantsObject);
+
+	user.editCourse(courseObject);
+	
+}
