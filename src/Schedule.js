43c7436685eda8472f8dc67177312c145aea9b69
Mehmet Ali ERGÜL <mehmetergul@std.iyte.edu.tr>
2019-06-02 16:54:22 +0300

    schedule-class

--- src/Schedule.js
+++ src/Schedule.js
@@ -1,5 +1,8 @@
 module.exports = class Schedule {
-
-
-	
+	constructor(courseCode,courseDay,courseTime,semester){
+		this.courseCode=courseCode;
+		this.courseDay=courseDay;
+		this.courseTime=courseTime;
+		this.semester=semester;
+	}
 }
