da8628158692d87cc3e46aaf21b5510edbcedfac
Furkan Sahin <mehmetergul@std.iyte.edu.tr>
2019-06-14 21:15:24 +0300

    fullpublish

--- src/APIHandler.js
+++ src/APIHandler.js
@@ -3,7 +3,9 @@ var WPAPI = require('wpapi');
 
 module.exports = class APIHandler {
 	
-	publishScheduleAPI(content){
+	
+	
+	publishScheduleAPI(weeklySchedule){
 		// You must authenticate to be able to POST (create) a post
 		var apiPromise = WPAPI.discover( 'http://ceng316group7.atwebpages.com' ).then(function( site ) {
 		    return site.auth({
@@ -17,7 +19,7 @@ module.exports = class APIHandler {
 		    site.pages().id( 11 ).update({
 		   	 // "title" and "content" are the only required properties
 		    	title: 'Weekly Course Schedule',
-		    	content: content,
+		    	content: weeklySchedule,
 		    	// Post will be created as a draft by default if a specific "status"
 		    	// is not specified
 		    	status: 'publish'
@@ -25,11 +27,64 @@ module.exports = class APIHandler {
 		    	// "response" will hold all properties of your newly-created post,
 		    	// including the unique `id` the post was assigned on creation
 		    	console.log( response.id );
-				alert("Weekly Course Schedule Published")
+				
+				return apiPromise;	
+			}).then(function( site ) {
+				//DELETE OLD COURSES PAGE
+				getAll( site.pages() ).then(function( allPages ) {
+		            allPages.forEach(function(page) {
+						if(page.slug != "courses" && page.slug != "weekly-course-schedules"){
+							site.pages().id( page.id ).delete().then(function( result ) {
+								console.log(result)
+							})
+						}
+					});
+				});
+				return apiPromise;
+			}).then(function( site ) {
+				//add new pages
+				var wrapperChildren = $("#hiddenCourses").children();
+
+				for (var i = 0; i < wrapperChildren.length; i++) {
+				    console.log();
+					site.pages().create({
+					    // "title" and "content" are the only required properties
+					    title: 'CENG'+$(wrapperChildren[i]).attr('id'),
+					    content: $(wrapperChildren[i]).html(),
+					    // Post will be created as a draft by default if a specific "status"
+					    // is not specified
+						slug: 'ceng-'+$(wrapperChildren[i]).attr('id'),
+					    status: 'publish'
+					}).then(function( response ) {
+					    // "response" will hold all properties of your newly-created post,
+					    // including the unique `id` the post was assigned on creation
+					    console.log( response.id );
+					})
+				}
+				
+				return apiPromise;
+			}).then(function( site ) {
+				//Update Courses pages
+				site.pages().id( 18 ).update({
+				    // "title" and "content" are the only required properties
+				    content: $("#coursesDiv").html(),
+				    // Post will be created as a draft by default if a specific "status"
+				    // is not specified
+				    status: 'publish'
+				}).then(function( response ) {
+				    // "response" will hold all properties of your newly-created post,
+				    // including the unique `id` the post was assigned on creation
+				    console.log( response.id );
+				})
+				
+				return 1;
 			}).catch(function(error) {
 	  		  	console.log(error);
+				return 0;
 			});
+			
 		})
+		
 	}
 	
 }
--- src/WebAdmin.js
+++ src/WebAdmin.js
@@ -39,14 +39,83 @@ module.exports = class WebAdmin extends ContentManager{
 		//Now add the new oness
 		var result = DBHandler.getScheduleDB(semester);
 		result.then(function(courseObjects) {
+					console.log(courseObjects)
+			
 			courseObjects.forEach(function(Course) {
 				Course.schedule.forEach(function(schedule) {
 					let year = Math.round(Course.semester/2);
 					//First Year Table
 					$("#year"+year+" tr").eq(schedule.courseTime)[0].cells[schedule.courseDay].innerHTML = "CENG"+schedule.courseCode;
 				});
-			});			
+			});	
+			return courseObjects;
+		}).then(function(courseObjects) {
+			//First Reset the DIV
+			$("#hiddenCourses").html("");
+			//Add the new courses
+			courseObjects.forEach(function(Course) {
+				let courseStatus = "Offered";
+				if(Course.active == 0) courseStatus = "Inactive"
+				let courseHTML = `
+					<div id="${Course.courseCode}">
+					<!-- wp:paragraph -->
+					<p><strong>${Course.courseName}</strong></p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:paragraph -->
+					<p><strong>Instructors:</strong></p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:list -->
+					<ul>`;
+					Course.instructors.forEach(function(instructor) {
+						courseHTML += `<li>${instructor}</li>`
+					});
+					courseHTML += `
+					</ul>
+					<!-- /wp:list -->
+
+					<!-- wp:paragraph -->
+					<p><strong>Assistants:</strong></p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:list -->
+					<ul>`;
+					Course.assistants.forEach(function(assistant) {
+					courseHTML += `<li>${assistant}</li>`
+					});
+					courseHTML += `
+					</ul>
+					<!-- /wp:list -->
+
+					<!-- wp:paragraph -->
+					<p><strong>Status:</strong> ${courseStatus}</p>
+					<!-- /wp:paragraph -->
+					</div>`;
+					
+					$("#hiddenCourses").append(courseHTML);
+			});	
+			return courseObjects;
+		}).then(function(courseObjects) {
+			//first delete old courses div
+			$("#coursesDiv").html("");
+			
+			//now add new one
+			let appendCoursesDiv = `
+			<!-- wp:list -->
+			<ul>`;
+			courseObjects.forEach(function(Course) {
+				appendCoursesDiv += `<li><a href="/ceng-${Course.courseCode}/">CENG ${Course.courseCode}</a></li>`
+			});	
+			appendCoursesDiv +=`
+			</ul>
+			<!-- /wp:list -->`;
+			console.log(appendCoursesDiv)
+			$("#coursesDiv").append(appendCoursesDiv);
+			return 1;
 		});
+		
+		
 	}
 	
 
